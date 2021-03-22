a012534eb4acd8d5670ede10fc6c274eb0a24b95
Gor1233 <81142306+Gor1233@users.noreply.github.com>
2021-03-22 10:59:16 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+ <?php include 'sendemail.php'; ?> 
+
+<!DOCTYPE html>
+<html lang= "en-US">
+<head>
+<meta content="width=device-width, initial-scale=1" name="viewport" />
+
+<title>Home - Mkrtchyan Production</title>    
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/progresbar.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div id="progressbar"></div>
+    <div id="scollPath"></div>
+    
+    <div class="banner">
+        <div class="navbar">
+            <a href="index.php"> <img src="css/images/mkrtchyan-production-logo.png"  class="logo"></a> 
+                <ul>
+                    <!-- <li class="active"><a href="#"><i class="fa fa-home"></i>Home</a></li> -->
+                    <!-- <li class="dropdownmobile"><a href="#"> <img src="images/menue.png"> </a>  </li> -->
+                    <li><a href="about.php"><i class="fa fa-users"></i>About Us</a></li>
+                    <li><a href="services.php"><i class="fa fa-align-center"></i>Services</a></li>
+                    <li><a href="contactus.php"><i class="fa fa-phone"></i>Contant</a></li>
+                    <li class="dropdown" ><a href="#"><i class="fa fa-language"></i>Language</a>
+                <div class="dropdown-content">
+                    <a href="ruindex.php"> <img src="css/images/free-icon-russia-197408.png"  class="logoflag">Русский </a> 
+                     <a href="hyindex.php"> <img src="css/images/armenia.png" class="logoflag" >Հայերեն</a>
+                </div></li>
+                 </ul>
+        </div>
+
+        <div class="content">
+            <h1>MAKING YOUR VIDEO <br>WHITE US</h1>
+            <p><i class="fa fa-angle-double-down"></i>  We are on social networks  <i class="fa fa-angle-double-down"></i>
+            </p>
+            <div>
+                <a href="https://www.youtube.com/c/GorProductions" target="_blank">
+                <button type="button" class="active" ><span></span><i class="fa fa-youtube-play"></i></button>
+                </a>
+                <a href="https://www.facebook.com/gorproductionsgm" target="_blank">
+                <button type="button"><span></span><i class="fa fa-facebook"></i></button>
+                </a>
+                <a href="https://www.instagram.com/gorproductionsgp/" target="_blank">
+                <button type="button"><span></span><i class="fa fa-instagram"></i></button>
+                </a>
+                
+            </div>
+        </div>
+    
+    </div>
+
+    
+</body>
+</html>
